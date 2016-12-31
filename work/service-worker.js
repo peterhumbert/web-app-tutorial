c572fb510da2e1b9228145b040e7f841175b3cc8
Peter Humbert <humbe037@umn.edu>
2016-12-30 21:48:49 -0600

    make the service worker cache the app shell components

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,5 +1,23 @@
 var cacheName = 'weatherPWA-step-6-1';
-var filesToCache = [];
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -9,4 +27,24 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
+});
+
+
+// "This code ensures that your service worker updates its
+// cache whenever any of the app shell files change. In
+// order for this to work, you'd need to increment the
+// cacheName variable at the top of your service worker file."
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
